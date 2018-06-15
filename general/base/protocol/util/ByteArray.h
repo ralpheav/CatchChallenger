0c9efc8f0de6d24de8f0eba35f9a3a61436d4836
ralph <ralphadp@hotmail.com>
2018-06-14 20:28:12 -0700

    Fix datastream and ByteArray

--- general/base/protocol/util/ByteArray.h
+++ general/base/protocol/util/ByteArray.h
@@ -4,13 +4,29 @@
 #include <vector>
 
 class ByteArray {
-    std::vector<unsigned char> storage;
+    std::vector<uint8_t> storage;
 public:
-    const char* constData() {
-        return storage.data();
+    const char* constData() const {
+        return (char *)(storage.data());
     }
-    size_t size() {
+
+    char* data() const {
+        return (char *)(storage.data());
+    }
+
+    size_t size() const {
        return storage.size();
+    }
+
+    void push_back(uint8_t byte) {
+        storage.push_back(byte);
+    }
+
+    std::vector<uint8_t> get() {
+        return storage;
+    }
+    std::vector<uint8_t>* ptrget() {
+        return &storage;
     }
 };
 
--- general/base/protocol/util/DataStream.h
+++ general/base/protocol/util/DataStream.h
@@ -2,11 +2,13 @@
 #include <memory>
 #include <cstdint>
 #include <cassert>
+#include "ByteArray.h"
 
 class DataStream {
 
 public:
     typedef std::vector<uint8_t> Storage; //std::vector<unsigned char>
+
     enum ByteOrder {
         BigEndian,
         LittleEndian
@@ -17,39 +19,64 @@ public:
         ReadPastEnd
     };
 
-    enum Flag {
+    enum AccessMode {
         ReadOnly,
         WriteOnly,
         ReadWrite
     };
 
 private:
-    std::shared_ptr<const Storage> m_ptr;
-    const Storage* m_data;
+    std::shared_ptr< Storage> m_ptr;
+    Storage* m_data;
     size_t m_idx;
     Status m_status;
     ByteOrder m_byteOrder;
     ByteOrder m_systemOrder;
-    Flag m_mode;
+    AccessMode m_mode;
 
     static ByteOrder systemByteOrder() {
         const uint32_t t = 1;
+
         return (reinterpret_cast<const uint8_t*>(&t)) ? LittleEndian : BigEndian;
     }
 
     bool has(size_t count) const {
+
         return m_idx + count <= m_data->size();
     }
 
     template <typename T> DataStream& read(T& i) {
+        if (has(sizeof(T)) && Ok == m_status) {
+            T result = *reinterpret_cast<const T*>(&(*m_data)[m_idx]);
+            m_idx += sizeof(T);
+            if (m_byteOrder != m_systemOrder) {
+                T tmp = 0;
+                for (uint8_t index = 0; index < sizeof(T); ++index) {
+                    tmp = (tmp << 8) | (result & 0xFF);
+                    result = result >> 8;
+                }
+                i = tmp;
+            } else
+                i = result;
+        } else {
+            m_status = ReadPastEnd;
+        }
+        return *this;
     }
 
     template <typename T> DataStream& write(const T& i) {
+        if (m_data && Ok == m_status) {
+            m_data->push_back(i);
+            m_idx++;
+        } else {
+            m_status = ReadPastEnd;
+        }
+        return *this;
     }
 
-
 public:
-    DataStream(const std::vector<uint8_t>* data, Flag mode) :
+
+    DataStream(std::vector<uint8_t>* data, AccessMode mode) :
         m_data(data),
         m_idx(0),
         m_status(Ok),
@@ -57,6 +84,48 @@ public:
         m_systemOrder(systemByteOrder()),
         m_mode(mode) {
 
+    }
+
+    DataStream(std::shared_ptr<Storage> data, AccessMode mode) :
+        m_ptr(data),
+        m_data(m_ptr.get()),
+        m_idx(0),
+        m_status(Ok),
+        m_byteOrder(BigEndian),
+        m_systemOrder(systemByteOrder()),
+        m_mode(mode) {
+
+    }
+
+    DataStream(std::unique_ptr<Storage> && data, AccessMode mode) :
+        m_ptr(data.release()),
+        m_data(m_ptr.get()),
+        m_idx(0),
+        m_status(Ok),
+        m_byteOrder(BigEndian),
+        m_systemOrder(systemByteOrder()),
+        m_mode(mode) {
+
+    }
+
+    DataStream(Storage && data, AccessMode mode) :
+        m_ptr(new Storage(std::move(data))),
+        m_data(m_ptr.get()),
+        m_idx(0),
+        m_status(Ok),
+        m_byteOrder(BigEndian),
+        m_systemOrder(systemByteOrder()),
+        m_mode(mode) {
+
+    }
+
+    DataStream(ByteArray data, AccessMode mode) :
+        m_data(new Storage(data.get())),
+        m_idx(0),
+        m_status(Ok),
+        m_byteOrder(BigEndian),
+        m_systemOrder(systemByteOrder()),
+        m_mode(mode) {
     }
 
     bool atEnd() const {
