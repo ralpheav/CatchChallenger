a7b1e8d94cad7dda34aa6d74d2aa108d0116c32f
ralph_ <ralphadp@hotmail.com>
2018-08-01 01:03:15 -0400

    Default following resource

---
+++ general/base/UMapDecorator.h
@@ -0,0 +1,20 @@
+#ifndef UMAPDECORATOR_H
+#define UMAPDECORATOR_H
+
+#include <unordered_map>
+
+namespace CatchChallenger {
+
+    template<class Key, class Value>
+    class UMapDecorator : public std::unordered_map<Key, Value>
+    {
+        public:
+            bool exists(const Key& key) const
+            {
+                return this->find(key) != this->cend();
+            }
+    };
+
+}
+
+#endif // UMAPDECORATOR_H
